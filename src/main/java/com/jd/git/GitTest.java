85b3c3dac9be32b8cd221dc3ab433126996201f9
wtd5656 <wy13273819650@163.com>
2022-05-10 11:43:40 +0800

    first commit

---
+++ src/main/java/com/jd/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jd.git;
+
+/**
+ * @author wangyu
+ * @create 2022-05-10 11:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+}
